8e316c119682340154d69b60a0383b53fc030355
Choe, Cheng-Dae <whitekid@gmail.com>
2012-11-15 00:32:53 +0900

    set dhcp listen interface

--- cookbooks/dhcp/recipes/server.rb
+++ cookbooks/dhcp/recipes/server.rb
@@ -5,6 +5,23 @@ service "isc-dhcp-server" do
 	action [:enable, :restart]
 end
 
+case node[:hostname]
+when 'pxe'
+	interfaces = 'eth0'
+when 'router-data'
+	interfaces = 'eth1'
+else
+	interfaces = ''
+end
+
+template "/etc/default/isc-dhcp-server" do
+	source "default_isc-dhcp-server.erb"
+	variables({
+		:interfaces => interfaces,
+	})
+	notifies :restart, resources(:service => "isc-dhcp-server")
+end
+
 # @note pxe는 eth0에서 listen, 나머지는 eth1에서 listen
 template "/etc/dhcp/dhcpd.conf" do
 	source "dhcpd.conf.erb"
