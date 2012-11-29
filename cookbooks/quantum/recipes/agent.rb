403158995aea5e98c352fd70366f1c18370c7a83
Choe, Cheng-Dae <whitekid@gmail.com>
2012-11-30 01:25:41 +0900

    use domain name rather then ip

--- cookbooks/quantum/recipes/agent.rb
+++ cookbooks/quantum/recipes/agent.rb
@@ -11,11 +11,16 @@ rabbit_host = get_roled_host('openstack-rabbitmq')
 
 #
 # network connectivity
-# eth1: data-networks with dhcp enabled
+# eth1: data-networks
 #
-execute "data nic bring up" do
-	command "dhclient eth1"
-	not_if "ifconfig eth1 | grep 'inet addr'"
+eth0 = iface_addr(node, 'eth0').split('.')
+eth1 = eth0
+eth1[1] = '130'
+eth1 = eth1.join('.')
+
+ifconfig eth1 do
+	device 'eth1'
+	mask '255.255.255.0'
 end
 
 # setup bridge
@@ -32,8 +37,7 @@ template "/etc/quantum/plugins/openvswitch/ovs_quantum_plugin.ini" do
 		"enable_tunneling" => true,
 		"tenant_network_type" => 'gre',
 		"tunnel_id_ranges" => '1:1000',
-		"local_ip" => node['eth1'],
-		"local_ip" => %x(until ifconfig eth1 | grep 'inet addr' > /dev/null; do dhclient eth1 > /dev/null; sleep 1; done; ifconfig eth1 | grep 'inet addr' | cut -d : -f 2 | awk '{print $1}'),
+		"local_ip" => eth1,
 	})
 	notifies :restart, "service[quantum-plugin-openvswitch-agent]"
 end
